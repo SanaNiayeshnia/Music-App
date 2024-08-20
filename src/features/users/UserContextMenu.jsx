2ced77596521b09d75faf570188553acb642a565
Sana Niayeshnia <niayeshnia1381@gmail.com>
2024-08-20 19:52:31 +0330

    scroll main to top when clicking on profile

--- src/features/users/UserContextMenu.jsx
+++ src/features/users/UserContextMenu.jsx
@@ -2,16 +2,21 @@ import { TbLogout, TbUser } from "react-icons/tb";
 import ContextMenu from "../../ui/ContextMenu";
 import { useNavigate } from "react-router-dom";
 import useLogout from "./useLogout";
+import useMainContext from "../../ui/layout/useMainContext";
 
 function UserContextMenu({ children }) {
   const navigate = useNavigate();
   const { logout } = useLogout();
+  const { scrollMainToTop } = useMainContext();
 
   const options = [
     {
       title: "Profile",
       icon: <TbUser />,
-      handler: () => navigate("/account"),
+      handler: () => {
+        scrollMainToTop();
+        navigate("/account");
+      },
       closeAfterClick: true,
     },
     {

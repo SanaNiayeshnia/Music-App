3e01302d57217d2237b2832d0f966e26f345f129
Sana Niayeshnia <niayeshnia1381@gmail.com>
2024-08-30 18:37:18 +0330

    /

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,4 +1,4 @@
 export const APP_NAME = "Music App";
 export const CLIENT_ID = "bd27c74edd8143ec80babec902ce0639";
-export const REDIRECT_URI = "http://localhost:5173";
+export const REDIRECT_URI = "music-app-by-sana.netlify.app";
 export const CLIENT_SECRET = "15d4d275ad8c459ab98b62894e1e0da0";

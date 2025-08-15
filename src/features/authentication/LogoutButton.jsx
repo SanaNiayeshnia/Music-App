89b48436f7361bc6245b457d7870fbbc97de9587
Sana Niayeshnia <niayeshnia1381@gmail.com>
2025-08-15 19:59:27 +0330

    disconnect from Spotify on logout

--- src/features/authentication/LogoutButton.jsx
+++ src/features/authentication/LogoutButton.jsx
@@ -1,11 +1,17 @@
 import { TbLogout } from "react-icons/tb";
 import useLogout from "./hooks/useLogout";
+import { usePlayerContext } from "../../contexts/player/usePlayerContext";
 
 function LogoutButton() {
   const { logout } = useLogout();
+  const { player } = usePlayerContext();
+  function onLogout() {
+    player.disconnect();
+    logout();
+  }
   return (
     <TbLogout
-      onClick={logout}
+      onClick={onLogout}
       className="h-7 w-7 cursor-pointer text-black duration-100 hover:text-blue-600 md:hidden dark:text-white"
     />
   );

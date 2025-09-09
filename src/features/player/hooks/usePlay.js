4239a32b50175ba9d60dfc0539fd7ba85ea7d191
Sana Niayeshnia <niayeshnia1381@gmail.com>
2025-09-09 16:37:38 +0330

    make some changes

--- src/features/player/hooks/usePlay.js
+++ src/features/player/hooks/usePlay.js
@@ -1,13 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { play } from "../../../services/playerApi";
 import toast from "react-hot-toast";
 import { usePlayerContext } from "./usePlayerContext";
 
 function usePlay() {
+  const queryClient = useQueryClient();
   const { deviceId } = usePlayerContext();
   const { isPending, mutate: playMutate } = useMutation({
     mutationKey: ["play"],
     mutationFn: (data) => play({ ...data, deviceId: deviceId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["recently-played"]);
+    },
     onError: (error) => {
       toast.error(error?.message);
     },
--- src/features/tracks/PlayingTrack.jsx
+++ src/features/tracks/PlayingTrack.jsx
@@ -7,12 +7,12 @@ import { usePlayerContext } from "../player/hooks/usePlayerContext";
 function PlayingTrack() {
   const [isImageLoaded, setIsImageLoaded] = useState(false);
   const navigate = useNavigate();
-  const { currentTrack } = usePlayerContext();
+  const { currentTrack, playerState } = usePlayerContext();
 
   useEffect(() => {
     //if the currently playing song changed, set isImageLoaded to false and show the skeleton before loading the new image
     setIsImageLoaded(false);
-  }, [currentTrack?.id]);
+  }, [currentTrack?.id, playerState?.loading]);
 
   return (
     <div className="space-y-3">
